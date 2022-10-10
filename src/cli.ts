0d2f07cb4e67115793c1ffcbc2b9d607f605cdee
Binh Bui <binhbv@gmail.com>
2022-10-10 14:52:24 +0700

    basic tool

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,40 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line
-const importLocal = require('import-local');
+import inquirer from 'inquirer';
+import { A11yConverter } from './index';
+import { writeFileSync } from 'fs';
 
-if (!importLocal(__filename)) {
-
-}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'url',
+      message: 'Enter the URL to convert:',
+      validate: (input: string) => {
+        if (!input) {
+          return 'Please enter a valid URL';
+        }
+        return true;
+      },
+    },
+  ])
+  .then((answers) => {
+    const { url } = answers;
+    const converter = new A11yConverter();
+    converter
+      .convert({
+        url,
+        method: 'GET',
+      })
+      .then((html) => {
+        writeFileSync('output.html', html);
+      });
+  })
+  .catch((error) => {
+    console.error(error);
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
