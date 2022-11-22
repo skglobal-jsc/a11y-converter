6ce4b23b117404639aef27d8d43a405f393f0c90
pnson <43614413+pns1681999@users.noreply.github.com>
2022-11-22 11:12:10 +0700

    update: function buildMetaOption

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -57,20 +57,24 @@ export const buildMetaOptions = (opt: {
   type?: string;
 }): MetaOptions => {
   const metaOptions: MetaOptions = {
-    lang: opt?.lang ?? '',
-  };
-  metaOptions.meta = {
-    ...(!!opt.description && { description: opt.description }),
-    ...(!!opt.keywords && { keywords: opt.keywords }),
-  };
-  metaOptions.socialMeta = {
-    ...(!!opt.title && { 'og:title': opt.title }),
-    ...(!!opt.description && { 'og:description': opt.description }),
-    ...(!!opt.image && { 'og:image': opt.image }),
-    ...(!!opt.type && { 'og:type': opt.type }),
-  };
-  !!opt.title && (metaOptions.title = opt.title);
-  !!opt.favicon && (metaOptions.favicon = opt.favicon);
+    ...(!!opt.lang && { lang: opt.lang }),
+    ...(!!opt.title && { title: opt.title }),
+    ...(!!opt.favicon && { favicon: opt.favicon }),
+    meta: {
+      ...(!!opt.description && { description: opt.description }),
+      ...(!!opt.keywords && { keywords: opt.keywords }),
 
+      ...(!!opt.title && { 'twitter:title': opt.title }),
+      ...(!!opt.description && { 'twitter:description': opt.description }),
+      ...(!!opt.image && { 'twitter:image': opt.image }),
+      ...(!!opt.image && { 'twitter:card': 'summary_large_image' }),
+    },
+    socialMeta: {
+      ...(!!opt.title && { 'og:title': opt.title }),
+      ...(!!opt.description && { 'og:description': opt.description }),
+      ...(!!opt.image && { 'og:image': opt.image }),
+      ...(!!opt.type && { 'og:type': opt.type }),
+    },
+  };
   return metaOptions;
 };
