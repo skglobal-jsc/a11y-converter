f1570b468ae8c69d14c18d346e9bcfdb2464b8e1
Bao Dang <dxhbao@sk-global.biz>
2023-01-17 09:39:53 +0700

    Chore

--- src/modules/ragtJson2A11Y.ts
+++ src/modules/ragtJson2A11Y.ts
@@ -1,17 +1,15 @@
+import { ragtJson2A11Y as ragtJson2a11yClient } from '../utils/for-client/converter';
+import { ragtJson2A11Y as ragtJson2a11yServer } from '../utils/for-server/converter';
+
 const ragtJson2A11Y = ({
   env = 'server',
   ragtJson,
   metaOpt = {},
   playerBarOption,
 }) => {
-  if (env === 'server') {
-    return import('../utils/for-server/converter.js').then(
-      ({ ragtJson2A11Y }) => ragtJson2A11Y(ragtJson, metaOpt)
-    );
-  }
-  return import('../utils/for-client/converter.js').then(({ ragtJson2A11Y }) =>
-    ragtJson2A11Y(ragtJson, metaOpt, playerBarOption)
-  );
+  return env === 'server'
+    ? ragtJson2a11yServer(ragtJson, metaOpt)
+    : ragtJson2a11yClient(ragtJson, metaOpt, playerBarOption);
 };
 
 export default ragtJson2A11Y;
