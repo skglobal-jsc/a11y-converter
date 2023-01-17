e51ab9e89e9a7c09eff48f9db910ea91ddb8de5b
Bao Dang <dxhbao@sk-global.biz>
2023-01-17 16:10:52 +0700

    Chore: Delete some files

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ragtJson2A11Y,
-  // editorJson2RagtJson
-} from './modules';
-
-export {
-  ragtJson2A11Y,
-  // editorJson2RagtJson
-};
--- src/modules/index.ts
+++
@@ -1,4 +0,0 @@
-import ragtJson2A11Y from './ragtJson2A11Y';
-import editorJson2RagtJson from './editorJson2RagtJson';
-
-export { ragtJson2A11Y, editorJson2RagtJson };
