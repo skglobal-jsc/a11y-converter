39e0e21f18ff46a1c83f81beaf6aba2fa82e6957
Bao Dang <dxhbao@sk-global.biz>
2023-03-02 17:12:41 +0700

    Improve lib

--- dev.ts
+++ dev.ts
@@ -73,7 +73,7 @@ import {
 
 (async () => {
 
-  const {html} = await fromUrl({ url: 'https://www.town.horokanai.hokkaido.jp/archives/43800', opt: { contentSelectors: ['#post-43800 > div > div > p:nth-child(4)'] }})
+  const {html} = await fromUrl({ url: 'https://www.town.horokanai.hokkaido.jp/archives/43800', opt: { contentSelectors: ['#post-43800 > div > div'] }})
   html2Text({
     html,
     contentSelectors: ['body'],
