7c0308a177144e79d6a44e03133d5f5e4b3d6bcf
Binh Bui <bvbinh@sk-global.biz>
2025-04-22 09:35:14 +0700

    test case

--- test/test.ts
+++ test/test.ts
@@ -1,18 +1,31 @@
-// import { A11yConverter } from '../src';
+import { tinyhtml, html2Text } from '../src';
 
-// jest.useRealTimers();
+jest.useRealTimers();
 
-// describe('A11y Converter ', () => {
-//   it('should be a class', () => {
-//     expect(new A11yConverter()).toBeInstanceOf(A11yConverter);
-//   });
+const html = `
+<html>
+  <body>
+    <h1>Test</h1>
+    <span>Audio</span> <audio src="https://example.com/audio.mp3"></audio>
+    <span>Video</span> <video src="https://example.com/video.mp4"></video>
+  </body>
+</html>
+`;
 
-//   it('should return an object', async () => {
-//     const converter = new A11yConverter();
-//     const result = await converter.convert({
-//       url: 'https://skg-development-dev.s3.ap-southeast-1.amazonaws.com/public/original.html',
-//       method: 'GET',
-//     });
-//     expect(result).toBeInstanceOf(Object);
-//   });
-// });
+describe('Test tinyhtml', () => {
+  it('should parse audio and video tags', async () => {
+
+    const { html: simplifiedHTML } = await tinyhtml(html);
+    console.log(simplifiedHTML);
+    expect(simplifiedHTML).toContain('<span>Audio</span> <span>https://example.com/audio.mp3</span>');
+    expect(simplifiedHTML).toContain('<span>Video</span> <span>https://example.com/video.mp4</span>');
+  });
+});
+
+describe.skip('Test html2Text', () => {
+  it('should parse audio and video tags', async () => {
+    const { plainText, a11yHTML } = await html2Text({ html, iArticle: {} as any });
+    console.log(plainText, a11yHTML);
+    expect(plainText).toContain('TestAudiohttps://example.com/audio.mp3Videohttps://example.com/video.mp4');
+  });
+});
